1704ede129c4458f18cb69ece890dd15dab24e00
skwaweru254 <skwaweru254@gmail.com>
2023-01-26 13:37:51 +0300

    We are here

--- gui.py
+++ gui.py
@@ -2,10 +2,32 @@
 
 import tkinter
 
-top = tkinter.Tk()
+gui = tkinter.Tk()
 
 # You app widgets will go here, object too
 
+# Adding a label widget
+
+label = tkinter.Label(gui, text = "")
+
+# Adding a button widget
+
+bt = tkinter.Button (gui, text="Entered", bg="yellow", fg="black")
+bt.grid (column=1, row=0)
+
+# Gui title
+
+gui.title("We are here - Tkinter")
+
+# Configure background color
+
+gui.configure(background="orange")
+
+# Set window size
+
+gui.geometry('500x400')
+
+
 # Event mainloop method
 
-top.mainloop()
+gui.mainloop()
